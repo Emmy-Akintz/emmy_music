42cd457ee8f2eb62799ff6c3a58593376abbdcc6
Ayomide Akintan <emzyakints2005@gmail.com>
2023-06-10 13:51:34 +0100

    Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className='bg-info'>
       <BrowserRouter>
         <Routes>
           <Route path='/home' element={<Home />}></Route>
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function profile() {
   return (
-    <div>profile</div>
+    <div className=''>
+      <h1>Display your spotify profile data</h1>
+
+      <section id="profile">
+        <h2>Logged in as <span id="displayName"></span></h2>
+        <span id="avatar"></span>
+        <ul>
+          <li>User ID: <span id="id"></span></li>
+          <li>Email: <span id="email"></span></li>
+          <li>Spotify URI: <a href="#" id="uri"></a></li>
+          <li>Link: <a href="#" id="url"></a></li>
+          <li>Profile Image: <span id="imgUrl"></span></li>
+        </ul>
+      </section>
+    </div>
   )
 }
 
